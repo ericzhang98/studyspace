6082049b150340e0cc574952eb90acdca2eae89b
Saurabh Kulkarni <saurabh.kulkarni997@gmail.com>
2017-02-11 17:45:51 -0800

    dynamic class list population

--- public/controllers/classes_bar_controller.js
+++ public/controllers/classes_bar_controller.js
@@ -1,36 +1,24 @@
 //***** General variables **************************/
-var me = {user_id: "id1"};
-var class_rooms = {} 	// class_id : class rooms
-var class_names = {} 	// class_id : class name
-var rooms = {}			// room_id : room
+//var me = {user_id: "id1"};
+//var class_rooms = {} 	// class_id : class rooms
+//var class_names = {} 	// class_id : class name
+//var rooms = {}			// room_id : room
 /**************************************************/
 // classes sidebar app
 var myApp = angular.module("classesApp", []);
 
 myApp.controller("classesController", function($scope) {
 
-  /*myScope = $scope;
-  $scope.classes = {
-    123:"CSE110",
-    345:"CSE140",
-    6543:"CSE131",
-    876:"PSY 2B"
-  };*/
-
+  $scope.class_names = {}; // class_id : class rooms 
+  $scope.rooms = {}; // room_id : room
   getClasses();
-
-
-  function setClasses( classNamesList ) {
-    console.log("Setting classes");
-    $scope.classes = classNamesList; 
-  }
 
   // - gets all class_ids for user
   // - delegates to getClass
   function getClasses() {
     console.log("Getting classes...")
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', "/get_classes/" + me.user_id, true); // responds with class_ids
+    xhr.open('GET', "/get_classes", true); // responds with class_ids
     xhr.send();
 
     xhr.onreadystatechange = function(e) {
@@ -71,12 +59,12 @@ myApp.controller("classesController", function($scope) {
 
         // store the class
         var response = JSON.parse(xhr.responseText);
-        class_names[class_id] = response.name;
-
-        console.log("class name is: " + response.name);
-
         // TODO: update UI to add the class
-        $scope.classes.push(class_names);
+        $scope.class_names[class_id] = response.name;
+        //class_names[class_id] = response.name;
+        
+        console.log("class name is: " + response.name);
+        $scope.$apply();
       }
     }
   }
