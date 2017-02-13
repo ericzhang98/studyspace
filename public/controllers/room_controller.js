9f8c5c06da47e9acf03459cb01909e6c69caea60
Isaac Wang <isaacwang4076@gmail.com>
2017-02-12 17:16:47 -0800

    troll

--- public/controllers/room_controller.js
+++ public/controllers/room_controller.js
@@ -8,7 +8,8 @@ var config = {
 };
 firebase.initializeApp(config);
 var databaseRef = firebase.database().ref(); //root
-var chatDatabase = databaseRef.child("RoomMessages").child(currRoomID);
+var roomID = "cse110_asdf";
+var chatDatabase = databaseRef.child("RoomMessages").child(roomID);
 
 //Room app
 var myApp = angular.module("roomApp", []);
@@ -24,7 +25,7 @@ myApp.controller("ChatController", ["$scope", "$http",
       function uploadMessage(chatInput) {
         console.log("Sending chat with: " + chatInput);
         //var newChatMessage = new ChatMessage(name, email, chatInput, roomID, Date.now()/1000);
-        var newChatMessage = {text: chatInput, roomID: currRoomID, timeSent: Date.now()/1000};
+        var newChatMessage = {text: chatInput, roomID: roomID, timeSent: Date.now()/1000};
         //chatDatabase.child(roomID).push().set(newChatMessage);
         $http.post("/send_room_message", newChatMessage);
         chatInputBox.value = "";
